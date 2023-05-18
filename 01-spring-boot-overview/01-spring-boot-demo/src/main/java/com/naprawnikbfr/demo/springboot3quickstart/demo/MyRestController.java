24d38ee9bc3bcc4647ffac0a639f40589d177419
BLAZEJ FRACZEK <fraczek.blazej@gmail.com>
2023-05-19 01:44:37 +0200

    02 commit after adding devtools dependency

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/naprawnikbfr/demo/springboot3quickstart/demo/MyRestController.java
@@ -0,0 +1,14 @@
+package com.naprawnikbfr.demo.springboot3quickstart.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+    //expose "/" endpoint and return "Hello world"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
