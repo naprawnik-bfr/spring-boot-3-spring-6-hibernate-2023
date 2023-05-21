114b5f3bfe418c40858c31e46ffa4b2aa62103e9
BLAZEJ FRACZEK <fraczek.blazej@gmail.com>
2023-05-21 19:05:02 +0200

    Setting up Spring Boot - Command Line App

---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/naprawnikbfr/demo/Application.java
@@ -0,0 +1,22 @@
+package com.naprawnikbfr.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	//bean for command line runner:
+	@Bean
+	public CommandLineRunner commandLineRunner (String[] args){
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+}
